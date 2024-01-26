fff1c8e4418341b3f5c6e5057b001c9e6c1e092a
DESKTOP-H3JE4PD\wiftex <wiftexoff@yandex.ru>
2024-01-27 03:23:02 +0500

    Init Vue.js project

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+  <div class="bg-gray-900 min-h-[100svh]">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+  * {
+  color: aliceblue;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+</style>
