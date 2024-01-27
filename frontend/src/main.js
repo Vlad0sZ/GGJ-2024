1d16709ff43b44321e9829374b553f200b81de2f
DESKTOP-H3JE4PD\wiftex <wiftexoff@yandex.ru>
2024-01-27 21:33:40 +0500

    Изменен url сервера

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,28 @@ import store from "@/store";
 import './index.css'
 import { VueSignalR } from '@dreamonkey/vue-signalr';
 import { HubConnectionBuilder } from '@microsoft/signalr';
+// import { initializeApp } from "firebase/app";
 
 const connection = new HubConnectionBuilder()
-    .withUrl('http://158.160.131.200:5555/hub')
+    .withUrl('/apiredirect/hub')
     .build();
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBeTl3RVzY5rO1UmzjIv3kzMLwIJUh-HRY",
+//     authDomain: "make-me-laugh-front.firebaseapp.com",
+//     projectId: "make-me-laugh-front",
+//     storageBucket: "make-me-laugh-front.appspot.com",
+//     messagingSenderId: "903708519860",
+//     appId: "1:903708519860:web:5f08a499545f0264b916e1"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 createApp(App)
     .use(VueSignalR, { connection })
