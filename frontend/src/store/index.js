8429dd8bd3a5d43b5e55255c5831979fbacc8fee
DESKTOP-H3JE4PD\wiftex <wiftexoff@yandex.ru>
2024-01-27 19:37:35 +0500

    Готовый фронт (пока без стилей)

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,38 @@
 import { createStore } from 'vuex'
+import router from "@/router";
 
 export default createStore({
   state: {
-    guid: '',
-    isGame: false
+    gameState: 0,
+    clickCount: 0,
+    commandId: 0,
+    actions : [],
+    guid: 0
   },
   getters: {
   },
   mutations: {
+    setState(state, gameState) {
+      state.gameState = gameState
+      state.clickCount = 0
+      console.log("state " + gameState)
+      router.push(gameState === 2 ? '/game' : gameState === 3 ? '/end' : '/')
+    },
+    setClickCount(state, count){
+      state.clickCount = count
+    },
+    setCommandId(state, id) {
+      state.commandId = id
+    },
+    setActions(state, actions) {
+      state.actions = actions
+    },
+    setGuid(state, guid){
+      state.guid = guid
+    }
   },
   actions: {
+
   },
   modules: {
   }
--- frontend/src/views/StartView.vue
+++ frontend/src/views/StartView.vue
@@ -1,6 +1,7 @@
 <script>
   import router from "@/router";
 
+
   export default {
     name: 'StartView',
     methods: {
@@ -12,10 +13,9 @@
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-[100svh]">
+  <div class="flex flex-col justify-center items-center h-[100dvh]">
     <div class="flex flex-col justify-center items-center bg-slate-800 gap-3 rounded-2xl p-32">
       <p class="font-bold">Ожидание...</p>
-      <button class="bg-fuchsia-700 hover:bg-fuchsia-800 focus:bg-fuchsia-900 py-3 px-10 rounded-2xl" @click="goToGame">Старт</button>
     </div>
   </div>
 </template>
